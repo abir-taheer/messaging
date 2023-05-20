6be4eb876dbbde5c79f4a8692526d4d8977b5997
Abir Taheer <abir@taheer.me>
2023-05-20 03:12:42 -0400

    stop writing schema to disk

--- apps/server/src/codegen.ts
+++ apps/server/src/codegen.ts
@@ -2,21 +2,13 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 import { makeExecutableSchema } from "@graphql-tools/schema";
 import { typeDefs } from "./graphql";
 import { printSchema } from "graphql";
-import { existsSync, mkdirSync, writeFileSync } from "fs";
 
 const schema = makeExecutableSchema({ typeDefs });
-
 const fullSchema = printSchema(schema);
-
-if (!existsSync("src/generated")) {
-  mkdirSync("src/generated");
-}
-
-writeFileSync("src/generated/schema.graphql", fullSchema);
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "src/generated/schema.graphql",
+  schema: fullSchema,
   generates: {
     "src/generated/graphql.ts": {
       plugins: ["typescript", "typescript-resolvers"],
