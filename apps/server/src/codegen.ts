128f948088eca29f585a424ca1fff876c7f991cb
Abir Taheer <abir@taheer.me>
2023-05-21 05:28:33 -0400

    fix codegen script

--- apps/server/src/codegen.ts
+++ apps/server/src/codegen.ts
@@ -1,14 +1,18 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 import { makeExecutableSchema } from "@graphql-tools/schema";
-import { typeDefs } from "./graphql";
+import { typeDefs } from "./graphql/type-defs";
 import { printSchema } from "graphql";
 import fs from "fs";
 
+// First make a dump of the schema
 const schema = makeExecutableSchema({ typeDefs });
 const fullSchema = printSchema(schema);
-
+if (!fs.existsSync("src/generated")) {
+  fs.mkdirSync("src/generated");
+}
 fs.writeFileSync("src/generated/schema.graphql", fullSchema);
 
+// Then return the config
 const config: CodegenConfig = {
   overwrite: true,
   schema: fullSchema,
